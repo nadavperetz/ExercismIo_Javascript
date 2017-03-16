87a89e3ad288c9d84a9c65534865a924a6b78269
nadavperetz <nadavperetz@gmail.com>
2017-03-16 16:26:59 -0300

    Hello

---
+++ rna-transcription/rna-transcription.js
@@ -0,0 +1,17 @@
+var DnaTranscriber = function() {};
+
+DnaTranscriber.prototype.toRna = function(dna_one, dna_two) {
+    if (dna_one.length != dna_two.length)
+        throw new Error('DNA strands must be of equal length.');
+    else{
+        var counter = 0;
+        for (var idx = 0; idx < dna_one.length; idx++){
+            if (dna_one[idx]  != dna_two[idx])
+                counter ++;
+        }
+
+    }
+    return counter;
+};
+
+module.exports = DnaTranscriber;
