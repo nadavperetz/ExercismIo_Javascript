61e448da906493a274b02f1641b547bf4fbcb62f
nadavperetz <nadavperetz@gmail.com>
2017-03-16 16:41:09 -0300

    submit ex 4

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,17 +1,18 @@
 var DnaTranscriber = function() {};
 
-DnaTranscriber.prototype.toRna = function(dna_one, dna_two) {
-    if (dna_one.length != dna_two.length)
-        throw new Error('DNA strands must be of equal length.');
-    else{
-        var counter = 0;
-        for (var idx = 0; idx < dna_one.length; idx++){
-            if (dna_one[idx]  != dna_two[idx])
-                counter ++;
-        }
+DnaTranscriber.prototype.toRna = function(dna_string) {
+    var transcription = {"G": "C", "C": "G", "T": "A", "A": "U"};
 
+    var rna_complement = "";
+
+    for (var idx = 0; idx < dna_string.length; idx++){
+        if (dna_string[idx] in transcription)
+            rna_complement += transcription[dna_string[idx]];
+        else
+            throw new Error('Invalid input')
     }
-    return counter;
+    return rna_complement;
+
 };
 
 module.exports = DnaTranscriber;
